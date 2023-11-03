82e1b84ede05bab06aac6f30258c50f1ed1fcc1e
ttwange <ttwange2@gmail.com>
2023-11-03 07:25:28 +0300

    Setting post publish to true by default

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app = FastAPI()
 class Post(BaseModel):
   title: str
   content: str
+  published: bool = True
 
 @app.get("/")
 def root():
@@ -18,7 +19,8 @@ def get_posts():
   return {"data":"Number of posts"}
 
 @app.post("/createposts")
-def create_posts(new_post = Post):
-  print(new_post)
-  return {"data" : "new post"}
+def create_posts(new_post : Post):
+  print(new_post.title)
+  print(new_post.content)
+  return {"data": "new post"}
 
