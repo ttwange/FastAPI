ec597dabefcf009ecc563f912c33683e057cf295
ttwange <ttwange2@gmail.com>
2023-11-03 07:29:17 +0300

    adding an optional field of rating to the schema of the api

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
-
+from typing import Optional
 
 app = FastAPI()
 
@@ -9,6 +9,7 @@ class Post(BaseModel):
   title: str
   content: str
   published: bool = True
+  rating: Optional[int] = None
 
 @app.get("/")
 def root():
@@ -22,5 +23,6 @@ def get_posts():
 def create_posts(new_post : Post):
   print(new_post.title)
   print(new_post.content)
+  print(new_post.published)
   return {"data": "new post"}
 
