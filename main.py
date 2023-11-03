abf7de7cea045c1b18a508a1b706d48f56ed5cec
ttwange <ttwange2@gmail.com>
2023-11-03 07:05:06 +0300

    Working on the fast api

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+# class Post(BaseModel):
+#   title:
+#   content:
 
 @app.get("/")
 def root():
@@ -14,4 +20,5 @@ def get_posts():
 @app.post("/createposts")
 def create_posts(payload: dict = Body(...)):
   print(payload)
-  return {"New_content" : f"title {payload['title']} and the content {payload['content']}"}
+  return {"New_content" : f"title: {payload['title']} and the content: {payload['content']}"}
+
