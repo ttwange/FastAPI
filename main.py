c05be2c659f5717d9cf39204a2e3406ffb8b0b6f
ttwange <ttwange2@gmail.com>
2023-11-04 07:29:24 +0300

    applying indexing on the posts

--- main.py
+++ main.py
@@ -23,11 +23,16 @@ def find_post(id):
     if p['id'] == id:
       return p
 
+def find_index_post(id):
+  for i, p in enumerate(my_posts):
+    if p['id'] == id:
+      return i
+
 @app.get("/posts")
 def get_posts():
   return {"data":my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post : Post):
  post_dict = post.dict()
  post_dict['id'] = randrange(0,3000000)
@@ -35,8 +40,11 @@ def create_posts(post : Post):
  return {"data": post_dict}
 
 @app.get("/posts/{id}")
-def get_post(id: int, response: Response):
+def get_post(id: int):
   post = find_post(id)
   if not post:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} not found")
   return {"post_detail": post} 
+
+@app.delete("/posts/{id}")
+def delete_post(id : int):
